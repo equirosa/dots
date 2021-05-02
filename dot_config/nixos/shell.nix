a6aca33ba9e3bb263aea20f5cde7408feb370008
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-01 23:35:26 -0600

    add apply flag to chezmoi edit abbr

--- dot_config/nixos/shell.nix
+++ dot_config/nixos/shell.nix
@@ -39,7 +39,7 @@
           chap = "chezmoi apply";
           chc = "chezmoi cd";
           chd = "chezmoi diff";
-          che = "chezmoi edit";
+          che = "chezmoi edit --apply";
           cat = "bat -p";
           dg = "dragon -a -x";
           yd = "youtube-dl";
