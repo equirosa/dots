beecc986c31f3f8a8f22153421d98ed43803368f
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-07 20:01:59 -0600

    correct completion setting

--- dot_config/nvim/lua/completion.lua
+++ dot_config/nvim/lua/completion.lua
@@ -1,6 +1,6 @@
 local utils = require('utils')
 
-utils.opt('o', 'completeopt', 'menuone,noinsert,noselect')
+vim.o.completeopt = "menuone,noselect"
 vim.cmd [[set shortmess+=c]]
 vim.g.completion_confirm_key = ""
 vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
