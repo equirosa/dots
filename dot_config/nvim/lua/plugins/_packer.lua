8162e76d9402afdc893511ed7e24b093cde66778
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-08 23:13:16 -0600

    nvim: modularize plugins stuff

--- dot_config/nvim/lua/plugins/_packer.lua
+++ dot_config/nvim/lua/plugins/_packer.lua
@@ -1,3 +1,13 @@
+-- Auto-install packer
+local fn = vim.fn
+local execute = vim.api.nvim_command
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+
 return require('packer').startup(function()
 	use {'wbthomason/packer.nvim', opt = true}
 
