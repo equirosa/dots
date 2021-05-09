4a5488c5682bde29e7b3e813e13f96304b31fbfe
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-09 16:20:45 -0600

    nvim: comment out jdtls stuff

--- dot_config/nvim/lua/plugins/_telescope.lua
+++ dot_config/nvim/lua/plugins/_telescope.lua
@@ -200,34 +200,34 @@ local M = {
 local finders = require("telescope.finders")
 local sorters = require("telescope.sorters")
 local pickers = require("telescope.pickers")
-require("jdtls.ui").pick_one_async = function(results, _, label_fn, cb)
-    local opts = no_preview()
-    pickers.new(opts, {
-        prompt_title = "LSP Code Actions",
-        finder = finders.new_table({
-            results = results,
-            entry_maker = function(line)
-                return {
-                    valid = line ~= nil,
-                    value = line,
-                    ordinal = label_fn(line),
-                    display = label_fn(line)
-                }
-            end
-        }),
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(
-                function()
-                    local selection = actions.get_selected_entry(prompt_bufnr)
-                    actions.close(prompt_bufnr)
+-- require("jdtls.ui").pick_one_async = function(results, _, label_fn, cb)
+--     local opts = no_preview()
+--     pickers.new(opts, {
+--         prompt_title = "LSP Code Actions",
+--         finder = finders.new_table({
+--             results = results,
+--             entry_maker = function(line)
+--                 return {
+--                     valid = line ~= nil,
+--                     value = line,
+--                     ordinal = label_fn(line),
+--                     display = label_fn(line)
+--                 }
+--             end
+--         }),
+--         attach_mappings = function(prompt_bufnr)
+--             actions.select_default:replace(
+--                 function()
+--                     local selection = actions.get_selected_entry(prompt_bufnr)
+--                     actions.close(prompt_bufnr)
 
-                    cb(selection.value)
-                end)
-            return true
-        end,
-        sorter = sorters.get_fzy_sorter()
-    }):find()
-end
+--                     cb(selection.value)
+--                 end)
+--             return true
+--         end,
+--         sorter = sorters.get_fzy_sorter()
+--     }):find()
+-- end
 
 return setmetatable({}, {
     __index = function(_, k)
