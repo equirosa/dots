3c24bda1bb6d728cd4558dcc2e820b638d5151e6
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-08 21:40:20 -0600

    nvim: add other dependencies for telescope

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,9 +5,14 @@ return require('packer').startup(function()
 	use {'sainnhe/gruvbox-material' }
 	use {'kyazdani42/nvim-web-devicons'}
 
+	-- Telescope
 	use {
 		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/popup.nvim'},{'nvim-lua/plenary.nvim'}}
+		requires = {
+			'nvim-lua/popup.nvim','nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-frecency.nvim',
+            "nvim-telescope/telescope-fzy-native.nvim"
+		}
 	}
 
 	use {'neovim/nvim-lspconfig'}
