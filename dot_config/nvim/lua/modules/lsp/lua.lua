cfab043734ac98e754cf250adc3028d1ff08849d
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-09 18:27:31 -0600

    nvim: slim down lua lsp config

--- dot_config/nvim/lua/modules/lsp/lua.lua
+++ dot_config/nvim/lua/modules/lsp/lua.lua
@@ -1,25 +1,8 @@
 local on_attach = require('modules.lsp.on_attach')
 local lsp_config = require('lspconfig')
 
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
-
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = vim.fn.stdpath('cache') ..
-                              '/nlua/sumneko_lua/lua-language-server'
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name ..
-                           "/lua-language-server"
-
 lsp_config.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    cmd = {'lua-language-server'},
     on_attach = on_attach,
     settings = {
         Lua = {
