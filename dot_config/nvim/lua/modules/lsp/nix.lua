b22771a8da843b1b8770cc6d28e0829315afd420
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-11 18:47:16 -0600

    setup rnix-lsp

--- dot_config/nvim/lua/modules/lsp/nix.lua
+++ dot_config/nvim/lua/modules/lsp/nix.lua
@@ -1,3 +1,5 @@
 local on_attach = require('modules.lsp.on_attach')
+local lsp_config = require('lspconfig')
 
-require'lspconfig'.rnix.setup {on_attach = on_attach}
+-- require'lspconfig'.rnix.setup {on_attach = on_attach}
+lsp_config.rnix.setup {}
