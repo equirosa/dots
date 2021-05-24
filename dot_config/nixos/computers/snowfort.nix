052eba2a73934977867b91a24d1782acbd16d0ef
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-23 21:53:19 -0600

    ❄️ move gpu stuff to submodule

--- dot_config/nixos/computers/snowfort.nix
+++ dot_config/nixos/computers/snowfort.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     ./global.nix
-    ../hardware/amdgpu.nix
+    ../hardware/gpu/amdgpu.nix
     ../hardware/zsa.nix
   ];
   networking.hostName = "snowfort";
