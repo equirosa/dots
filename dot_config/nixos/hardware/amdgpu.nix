40c9e55dbb86f405c8a3a02019fad5b9dc3b55a9
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-05-23 21:52:29 -0600

    ❄️ reenable amd gpu stuff

--- dot_config/nixos/hardware/amdgpu.nix
+++ dot_config/nixos/hardware/amdgpu.nix
@@ -4,8 +4,8 @@
   hardware.opengl = {
     extraPackages = with pkgs;
       [
-        #rocm-opencl-icd
-        #rocm-opencl-runtime
+        rocm-opencl-icd
+        rocm-opencl-runtime
         amdvlk
       ];
     extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
