54a7b5f2e667e3e9a32c0d6250533c0ab02c78ac
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-06-18 16:12:09 -0600

    fix firefox integration in sway

--- dot_config/nixos/graphical/wayland/sway.nix
+++ dot_config/nixos/graphical/wayland/sway.nix
@@ -31,6 +31,7 @@
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
         export QT_QPA_PLATFORM=wayland
         export _JAVA_AWT_WM_NONREPARENTING=1
+        export MOZ_DBUS_REMOTE=1
         export MOZ_ENABLE_WAYLAND=1
         systemctl --user import-environment
       '';
