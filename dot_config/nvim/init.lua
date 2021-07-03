afd4385bb96fd88d885a262afae00cfa331e12c8
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-03 15:35:56 -0600

    remove nvim config

--- dot_config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = ' '
-require('plugins')
-require('modules._settings')
-require('modules.theme')
-require('modules.lsp')
-require('modules.mappings')
--- dot_config/nvim/lua/modules/lsp/nix.lua
+++
@@ -1,3 +0,0 @@
-local lsp_config = require('lspconfig')
-
-lsp_config.rnix.setup {}
