1939981b427c92dead5a114a0d56f490c4e78d6f
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-05 12:30:47 -0600

    flatpak: remove aliases

--- dot_config/nixos/flatpak.nix
+++ dot_config/nixos/flatpak.nix
@@ -1,12 +1,4 @@
 { config, pkgs, ... }: {
-  environment = {
-    shellAliases = {
-      fi = "flatpak install";
-      fl = "flatpak list";
-      fs = "flatpak search";
-      fu = "flatpak uninstall";
-    };
-  };
   services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
