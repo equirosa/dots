9a8f237088b6b8ccc26a1b6f5878069bfdcc764a
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-09 00:47:09 -0600

    git: use delta

---
+++ dot_config/nixos/hardware/laptop.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }: {
+  services = {
+    xserver = {
+      libinput = {
+        enable = true;
+        touchpad = {
+          disableWhileTyping = true;
+        };
+      };
+    };
+  };
+}
