01e6e1cc7ad5ae913e9c25be705c53c2489b4c72
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-11 02:32:26 -0600

    enable xdg-portal-gtk

--- dot_config/nixos/flatpak.nix
+++ dot_config/nixos/flatpak.nix
@@ -4,7 +4,7 @@
     enable = true;
     extraPortals = with pkgs;
       [
-        #xdg-desktop-portal-gtk
+        xdg-desktop-portal-gtk
         #xdg-desktop-portal-kde
         #xdg-desktop-portal-wlr
       ];
