121818cb37c5e3b874ee1a4797ca13933f49c676
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-11 21:00:59 -0600

    wezterm: set font with fallback in case I ever wanna add one

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm';
 return {
     color_scheme = "Gruvbox Dark",
-    font = wezterm.font("FiraCode Nerd Font"),
+    font = wezterm.font_with_fallback({"FiraCode Nerd Font"}),
     window_background_opacity = 0.8
 }
