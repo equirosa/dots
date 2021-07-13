1fedf963edcc90282310c13ac02ec47653a0f0fe
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-13 16:56:56 -0600

    wezterm: set font size to 14

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,5 +2,6 @@ local wezterm = require 'wezterm';
 return {
     color_scheme = "Gruvbox Dark",
     font = wezterm.font_with_fallback({"FiraCode Nerd Font"}),
+    font_size = 14.0,
     window_background_opacity = 0.8
 }
