a3e0908fa7ccc1ec2a2bbd23a839a55264aed925
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-13 16:57:09 -0600

    wezterm: enable all harfbuzz features

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,5 +3,6 @@ return {
     color_scheme = "Gruvbox Dark",
     font = wezterm.font_with_fallback({"FiraCode Nerd Font"}),
     font_size = 14.0,
+    harfbuzz_features = {"calt=1", "clig=1", "liga=1"},
     window_background_opacity = 0.8
 }
