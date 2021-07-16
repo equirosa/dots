dc5bf913c2199dd397cd3a4d14832b927f9cfb9e
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-07-16 01:52:03 -0600

    neovim: use system package with override directly

--- dot_config/nixos/editors/neovim.nix
+++ dot_config/nixos/editors/neovim.nix
@@ -1,35 +1,14 @@
 { config, pkgs, ... }: {
   nixpkgs.overlays = [
-    (
-      import (
-        builtins.fetchTarball {
-          url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-        }
-      )
-    )
+    (self: super: {
+      neovim = super.neovim.override {
+        viAlias = true;
+        vimAlias = true;
+        withNodeJs = true;
+      };
+    })
   ];
-  home-manager.users.kiri = {
-    programs.neovim = {
-      enable = true;
-      package = pkgs.neovim-nightly;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      withNodeJs = true;
-      withPython3 = true;
-      extraConfig = ''
-        lua << EOF
-        ${builtins.readFile /home/kiri/.config/nvim/init.lua}
-        EOF
-      '';
-      plugins = with pkgs.vimPlugins; [
-        direnv-vim
-        fzf-vim
-        lazygit-nvim
-        lf-vim
-        neuron-vim
-        vim-nix
-      ];
-    };
-  };
+  users.users.kiri.packages = with pkgs; [
+    neovim
+  ];
 }
