3bdf1f346a83819163245b7bcfdd925320ac1a41
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-08-15 00:45:41 -0600

    add security config stuff (not in use yet)

---
+++ dot_config/nixos/security.nix
@@ -0,0 +1,11 @@
+{ config, ... }: {
+  security = {
+    # chromiumSuidSandbox.enable = true;
+    doas = {
+      enable = true;
+      extraConfig = "permit kiri as root";
+      extraRules = [{ keepEnv = true; }];
+    };
+    # unprivilegedUsernsClone = true;
+  };
+}
