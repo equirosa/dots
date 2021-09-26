23b85f86f535d7b6f12a82aea526ed51de5d8207
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-09-26 03:31:05 -0600

    de-import mysql stuff
    it was only necessary for a course

--- dot_config/nixos/computers/snowfort.nix
+++ dot_config/nixos/computers/snowfort.nix
@@ -4,7 +4,6 @@
     ../hardware/cpu/amd.nix
     # ../hardware/gpu/amdgpu.nix
     ../hardware/zsa.nix
-    ../databases/mysql.nix
   ];
   networking.hostName = "snowfort";
   zramSwap = { enable = true; };
