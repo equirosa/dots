d62c561cd5f6ef015ce374f10f4e1dc5aa91c10c
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-10-09 04:36:17 -0600

    create script to show ansi escapes and their effects

--- dot_config/nixos/packages/safe.nix
+++ dot_config/nixos/packages/safe.nix
@@ -114,5 +114,10 @@
       #!/bin/sh
       readlink -f "$(which "$1")"
     '')
+    (pkgs.writeScriptBin "show-ansi-escapes" ''
+      for (( i = 30; i < 38; i++ )); do
+        echo -e "\033[0;"$i"m Normal: (0;$i); \033[1;"$i"m Light: (1;$i);"
+        done
+    '')
   ];
 }
