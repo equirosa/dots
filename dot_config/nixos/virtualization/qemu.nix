78422236dfbbc425de5c02caae68d724d0529e6e
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-10-09 04:36:27 -0600

    update qemu nix config

--- dot_config/nixos/virtualization/qemu.nix
+++ dot_config/nixos/virtualization/qemu.nix
@@ -3,9 +3,11 @@
   virtualisation = {
     libvirtd = {
       enable = true;
+      qemuPackage = pkgs.qemu_kvm;
       qemuRunAsRoot = false;
     };
   };
+  programs.dconf.enable = true;
   users.users.kiri = {
     extraGroups = [ "libvirtd" ];
     packages = with pkgs; [ virt-manager ];
