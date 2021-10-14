01dbd143f105abae7c514b74471c3fe12410e1a3
Eduardo Quiros <eduardo@eduardoquiros.com>
2021-10-14 12:03:25 -0600

    clean up zathura config

--- dot_config/nixos/programs/zathura.nix
+++ dot_config/nixos/programs/zathura.nix
@@ -1,22 +1,20 @@
 { config, pkgs, ... }: {
-  home-manager.users.kiri = { pkgs, ... }: {
-    programs.zathura = {
-      enable = true;
-      extraConfig = ''
-        # Adjusting the document
-        map [normal] E adjust_window best-fit
-        map [fullscreen] E adjust_window best-fit
-        map [normal] e adjust_window width
-        map [fullscreen] e adjust_window width
+  home-manager.users.kiri.programs.zathura = {
+    enable = true;
+    extraConfig = ''
+      # Adjusting the document
+      map [normal] E adjust_window best-fit
+      map [fullscreen] E adjust_window best-fit
+      map [normal] e adjust_window width
+      map [fullscreen] e adjust_window width
 
-        # Toggling the inverted colours
-        map <C-i> recolor
-      '';
-      options = {
-        adjust-open = "width";
-        selection-clipboard = "clipboard";
-        window-title-basename = "true";
-      };
+      # Toggling the inverted colours
+      map <C-i> recolor
+    '';
+    options = {
+      adjust-open = "width";
+      selection-clipboard = "clipboard";
+      window-title-basename = "true";
     };
   };
 }
